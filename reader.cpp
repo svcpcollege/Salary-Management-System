4b3af9986a37f50b69359b9e2d8c441bde06a616
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2023-01-04 04:32:19 +0530

    Update reader.cpp

--- reader.cpp
+++ reader.cpp
@@ -7,14 +7,40 @@ using namespace std;
 
 class salary
 {
-    int Id; //1
-    string EmployeeName; //2
-    string JobTitle; //3
-    float BasePay, OvertimePay ,OtherPay ,Benefits ,TotalPay, TotalPayBenefits; //4-9
-    int Year; //10
-    string Notes; //11
-    string Agency; //12
-    string Status; //13
-public:
+    int ID;                                                                     // 1
+    string EmployeeName;                                                        // 2
+    string JobTitle;                                                            // 3
+    float BasePay, OvertimePay, OtherPay, Benefits, TotalPay, TotalPayBenefits; // 4-9
+    int Year;                                                                   // 10
+    string Notes;                                                               // 11
+    string Agency;                                                              // 12
+    string Status;                                                              // 13
+    ofstream fin;
 
+public:
+    float getSalaryByID(int ID = 1)
+    {
+        float sal = (float)NULL;
+        fin.open("Salaries.txt", ios::in);
+        if (!fin)
+        {
+            cout << "Error opening file\n";
+            return 0;
+        }
+
+
+
+
+
+
+        fin.close();
+        return sal;
+    }
 };
+
+int main()
+{
+    salary s;
+    cout << s.getSalaryByID(4);
+    return 0;
+}
